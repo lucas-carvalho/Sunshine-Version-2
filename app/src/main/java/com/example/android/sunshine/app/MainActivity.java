8800186370f20ae0dde68c10939e6c360a44c6d5
Lucas Carvalho <lucas.carvalho@evino.com.br>
2017-11-14 17:07:06 -0200

    fix: change class ActionBarActivity deprecated to AppCompatActivity in MainActivity

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
